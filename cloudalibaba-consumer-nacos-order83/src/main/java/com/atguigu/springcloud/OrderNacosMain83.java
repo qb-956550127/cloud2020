71cfed49f21a7c4fa8ac2f87b43a82ae890881ef
champion <956550127@qq.com>
2021-11-10 13:15:10 +0800

    git hub

--- cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/springcloud/OrderNacosMain83.java
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/springcloud/OrderNacosMain83.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderNacosMain83 {
 
     public static void main(String[] args) {
-        System.out.println("gitee");
+        System.out.println("github");
         SpringApplication.run(OrderNacosMain83.class,args);
     }
 }
