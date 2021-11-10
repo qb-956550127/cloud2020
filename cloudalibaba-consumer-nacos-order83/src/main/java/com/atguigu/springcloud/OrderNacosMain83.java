9356e5d9183b6f896828fff31751281942ffd148
champion <956550127@qq.com>
2021-11-10 16:53:29 +0800

    ceshi

--- cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/springcloud/OrderNacosMain83.java
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/springcloud/OrderNacosMain83.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class OrderNacosMain83 {
 
     public static void main(String[] args) {
-        System.out.println("git8888");
+        System.out.println("83");
         SpringApplication.run(OrderNacosMain83.class,args);
     }
 }
